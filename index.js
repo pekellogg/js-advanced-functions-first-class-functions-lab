ab6c282302aa5d7711cc50d3e78bcef96d68cbc8
Peyton Kellogg <pekellogg@hotmail.com>
2022-08-23 21:02:32 -0700

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(driversArr) {
+  return driversArr.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(driversArr) {
+  return driversArr.slice(-2);
+}
+
+const selectDifferentDrivers = function(driversArr, driversToReturn) {
+  return driversToReturn(driversArr);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integerArg = 1) {
+  return function(fare) {
+    return fare * integerArg;
+  }
+}
+
+// const fareDoubler = function(fare) {
+//   return createFareMultiplier(2)(fare);
+// }
+const fareDoubler = createFareMultiplier(2);
+
+// const fareTripler = function(fare) {
+//   return createFareMultiplier(3)(fare);
+// }
+const fareTripler = createFareMultiplier(3);
